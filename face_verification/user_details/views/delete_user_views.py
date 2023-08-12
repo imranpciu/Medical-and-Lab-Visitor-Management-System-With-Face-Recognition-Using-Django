d4a90c48c95768fa7f5049ef7bee800e4cdb64b8
Md Imran <79196192+imranpciu@users.noreply.github.com>
2023-08-12 16:27:47 +0600

    Delete face_verification directory

--- face_verification/user_details/views/delete_user_views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponseRedirect, reverse
-from django.views import View
-from user_details.model.add_user_models import User
-
-class DeleteUserView(View):
-    def post(self, request, user_id):
-        user = User.objects.get(id=user_id)
-        user.delete()
-        return HttpResponseRedirect(reverse("user_details:index"))  # Replace 'index' with the appropriate URL name for the index view
